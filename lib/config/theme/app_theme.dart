6ec2fbb031f5988f6fc320d3af9e3748d9f1a118
JuanCarlosTZ <juancarlos.tz@outlook.com>
2024-03-18 12:37:37 -0400

    Init proyect

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({required this.selectedColor})
+      : assert(selectedColor >= 0 && selectedColor < colorList.length,
+            'Color must be between 0 and ${colorList.length - 0}');
+
+  ThemeData getTheme() => ThemeData(
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
+}
+
+const List<Color> colorList = [
+  Colors.green,
+  Colors.teal,
+  Colors.pink,
+  Colors.red,
+  Colors.blue,
+  Colors.yellow,
+  Colors.purple,
+];
