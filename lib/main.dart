68fc02f7dbc95e05e8c18371174ad27eb46a067b
JuanCarlosTZ <juancarlos.tz@outlook.com>
2024-03-18 12:51:02 -0400

    Aplicar banner en false

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
       home: Scaffold(
         body: Center(
